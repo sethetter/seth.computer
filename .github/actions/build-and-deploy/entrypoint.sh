399f466cfc6be3d45ca4ffa8845013c9469d7340
Seth Etter <mail@sethetter.com>
2020-11-11 20:16:14 -0600

    Run the whole thing in entrypoint?

--- .github/actions/build-and-deploy/entrypoint.sh
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+curl -L https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz | tar xz -C /usr/local/bin
+
+# Install netlify
+npm i -g netlify-cli
+
+# Kick off build and deploy
 zola build
 netlify deploy \
   --prod \
